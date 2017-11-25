2f59b20eaeb5e475378f0364be192ecaf5ff889f
Jack P <jacp3@outlook.com>
2017-11-25 13:29:00 +0000

    fix info again

--- App.js
+++ App.js
@@ -27,7 +27,9 @@ export default class App extends React.Component {
               color='#000'
               onPress={() => {Alert.alert(
                   'Made by Jack Paine, 2017',
-                  'A simple Android app for testing the vibration motors on mobile phones.\n\nThis app is open-source. Click the GitHub icon in the left corner to view the source.'
+                  {'A simple Android app for testing the vibration motors on mobile phones.\n
+                  \n
+                  This app is open-source. Click the GitHub icon in the right corner to view the source.'}
               )}}
             />
         </View>
